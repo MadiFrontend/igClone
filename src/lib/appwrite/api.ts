5e166a65bf37efb2a7ce67bdc2a3f7973b191e02
madifrontend <madifrontend@gmail.com>
2023-10-30 10:38:10 +0330

    addDB

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,6 +1,7 @@
 import { INewUser } from "@/types";
 import { ID } from "appwrite";
-import { account } from "./Config";
+import { account, appwriteConfig, avatars, databases } from "./Config";
+import { error } from "console";
 
 export async function createUserAccount(user: INewUser) {
   try {
@@ -10,9 +11,38 @@ export async function createUserAccount(user: INewUser) {
       user.password,
       user.name
     );
-    return newAccount;
+    if (!newAccount) throw error;
+    const avatarUrl = avatars.getInitials(user.name);
+    const newUser = await saveUserToDb({
+      accountId: newAccount.$id,
+      email: newAccount.email,
+      name: newAccount.name,
+      username: user.username,
+      imageUrl: avatarUrl,
+    });
+    return newUser;
   } catch (error) {
     console.log(error);
     return error;
+  }
+}
+
+export async function saveUserToDb(user: {
+  accountId: string;
+  email: string;
+  name: string;
+  imageUrl: URL;
+  username?: string;
+}) {
+  try {
+    const newUser = await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      ID.unique(),
+      user
+    );
+    return newUser;
+  } catch (error) {
+    console.log(error);
   }
 }
